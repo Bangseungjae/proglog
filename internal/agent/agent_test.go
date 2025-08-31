e92cf6ff1266350155878b2f4dfb3cc43f30515a
Seungjae Bang <tmdwo5825@gmail.com>
2025-08-31 23:25:01 +0900

    test

---
+++ internal/agent/agent_test.go
@@ -0,0 +1,111 @@
+package agent
+
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/Bangseungjae/proglog/internal/config"
+	api "github.com/Bangseungjae/proglog/proto/api/v1"
+	"github.com/stretchr/testify/require"
+	"github.com/travisjeffery/go-dynaport"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+)
+
+const LocalAddr = "127.0.0.1"
+
+func TestAgent(t *testing.T) {
+	serverTLSConfig, err := config.SetupTLSConfig(config.TLSConfig{
+		CertFile:      config.ServerCertFile,
+		KeyFile:       config.ServerKeyFile,
+		CAFile:        config.CAFile,
+		Server:        true,
+		ServerAddress: LocalAddr,
+	})
+	require.NoError(t, err)
+
+	peerTLSConfig, err := config.SetupTLSConfig(
+		config.TLSConfig{
+			CertFile:      config.RootClientCertFile,
+			KeyFile:       config.RootClientKeyFile,
+			CAFile:        config.CAFile,
+			Server:        false,
+			ServerAddress: LocalAddr,
+		},
+	)
+	require.NoError(t, err)
+
+	var agents []*Agent
+	for i := 0; i < 3; i++ {
+		ports := dynaport.Get(2)
+		bindAddr := fmt.Sprintf("%s:%d", LocalAddr, ports[0])
+		rpcPort := ports[1]
+
+		dataDir, err := os.MkdirTemp("", "agent-test-log")
+		require.NoError(t, err)
+
+		var startJoinAddrs []string
+		if i != 0 {
+			startJoinAddrs = append(
+				startJoinAddrs,
+				agents[0].BindAddr,
+			)
+		}
+
+		agent, err := New(Config{
+			NodeName:        fmt.Sprintf("%d", i),
+			StartJoinAddrs:  startJoinAddrs,
+			BindAddr:        bindAddr,
+			RPCPort:         rpcPort,
+			DataDir:         dataDir,
+			ACLModeFile:     config.ACLModelFile,
+			ACLPolicyFile:   config.ACLPolicyFile,
+			ServerTLSConfig: serverTLSConfig,
+			PeerTLSConfig:   peerTLSConfig,
+		})
+		require.NoError(t, err)
+		agents = append(agents, agent)
+	}
+	defer func() {
+		for _, agent := range agents {
+			err := agent.Shutdown()
+			require.NoError(t, err)
+			require.NoError(t, os.RemoveAll(agent.Config.DataDir))
+		}
+	}()
+	time.Sleep(1 * time.Second)
+
+	leaderClient := client(t, agents[0], peerTLSConfig)
+	produceResponse, err := leaderClient.Produce(
+		context.Background(),
+		&api.ProduceRequest{
+			Record: &api.Record{
+				Value: []byte("foo"),
+			},
+		},
+	)
+	require.NoError(t, err)
+	consumeResponse, err := leaderClient.Consume(
+		context.Background(),
+		&api.ConsumeRequest{
+			Offset: produceResponse.Offset,
+		},
+	)
+	require.NoError(t, err)
+	require.Equal(t, consumeResponse.Record.Value, []byte("foo"))
+}
+
+func client(t *testing.T, agent *Agent, tlsConfig *tls.Config) api.LogClient {
+	tlsCreds := credentials.NewTLS(tlsConfig)
+	pts := []grpc.DialOption{grpc.WithTransportCredentials(tlsCreds)}
+	rpcAddr, err := agent.Config.RPCAddr()
+	require.NoError(t, err)
+	conn, err := grpc.NewClient(rpcAddr, pts...)
+	require.NoError(t, err)
+	client := api.NewLogClient(conn)
+	return client
+}
